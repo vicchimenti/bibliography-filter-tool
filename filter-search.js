22adc246ccb33e58a8e456fa4a38743617ce8b74
Vic Chimenti <chimenti@seattleu.edu>
2020-06-03 09:57:37 -0700

    type filter partially functional

--- filter-search.js
+++ filter-search.js
@@ -84,7 +84,7 @@ $(function () {
                     let typeKey = $(this).val();
                     // If Search Key is Not Null then Compare to the Type List Items in Each Content Item
                     if (typeKey) {
-                        $('.tag').filter(function (i, e) {
+                        $('.tags').filter(function (i, e) {
                             var typeValue = $(this).text();
                             // Check to see if the Key and Value are a Match
                             if (typeValue.match(typeKey)) {
