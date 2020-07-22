78522c4ad807a48ce9d8542bdd2bc1cb95b2c65d
Vic Chimenti <chimenti@seattleu.edu>
2020-07-22 07:30:36 -0700

    mash

--- filter-search.js
+++ filter-search.js
@@ -2,9 +2,6 @@
 /***
 *   @author Victor Chimenti, MSCS-SE 2020
 *   @file filter-search.js
-*   @see UCOR, UNIVERSITY CORE CURRICULUM, https://www.seattleu.edu/core/for-students/courses/
-*   as an example of this filter engine
-*   This URI will change after launch to to suit the host department
 *
 *   jQuery
 *   This script searches the Bibliography content items for matches to the
